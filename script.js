5c1f4e9ac47b98bfb62b5418108f9807287ae5b7
Chaisson <Chaissonrcook@gmail.com>
2026-04-01 13:17:34 -0700

    before

--- script.js
+++ script.js
@@ -7,26 +7,36 @@ function selectSex(btn, sex) {
 }
 
 function calculate() {
-  const weight = parseFloat(document.querySelector(".bodyWeight").value);
-  const heightFt = parseFloat(document.querySelector(".heightFt").value)
-  const heightIn = parseFloat(document.querySelector(".heightIn").value);
-  const height = (heightFt * 12) + heightIn;
+  const inputWeight = parseFloat(document.querySelector(".bodyWeight").value);
+  const inputHeightFt = parseFloat(document.querySelector(".heightFt").value)
+  const inputHeightIn = parseFloat(document.querySelector(".heightIn").value);
   const age = parseFloat(document.querySelector(".age").value);
-
   const activityFactor = parseFloat(document.querySelector(".activitySelect").value);
+  const heightIn = (inputHeightFt * 12) + inputHeightIn;
 
-  //Input Check
-  if (!weight || !height || !age || !selectedSex || !activityFactor) {
-    document.getElementById("result").textContent = "Please fill in all fields.";
-    return;
-  }
+  //convert to metric
+  const weightKg = (inputWeight / 2.2)
+  const heightCm = (heightIn * 2.54)
 
-  // Mifflin-St Jeor BMR (imperial: lbs & inches)
-  const bmrBase = (4.536 * weight) + (12.7 * height) - (5 * age);
+
+  // Mifflin-St Jeor BMR
+  const bmrBase = (10 * weightKg) + (6.25 * heightCm) - (5 * age);
   const bmr = selectedSex === "male" ? bmrBase + 5 : bmrBase - 161;
+
+  // BMI Calculation
+  const heightC = (heightCm/100)
+  const bmi = (weightKg/(heightC*heightC))
 
   const tdee = bmr * activityFactor;
 
+
   document.getElementById("result").textContent =
-    `BMR: ${Math.round(bmr)} cal/day | TDEE: ${Math.round(tdee)} cal/day`;
+    `BMI: ${(bmi.toFixed(1))} BMR: ${Math.round(bmr)} cal/day | TDEE: ${Math.round(tdee)} cal/day`;
+
+
+  //Input Check
+  if (!inputWeightweight || !inputHeight || !age || !selectedSex || !activityFactor) {
+    document.getElementById("result").textContent = "Please fill in all fields.";
+    return;
+  }
 }
